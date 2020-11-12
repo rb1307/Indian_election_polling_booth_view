2949c2eaea9ba38312054d5180ac4fc1ef736c4b
romit <romit.bhattacharyya@dataweave.com>
2020-11-12 19:25:01 +0530

    InputtypeError class added

--- Errors.py
+++ Errors.py
@@ -1,3 +1,8 @@
+class InputTypeError(Exception): 
+    def __init__(self, message="Error in the type of input file.Currently supported formats are only in csv and excel."):
+        self.message = message
+        super().__init__(self.message)
+
 class InputFormatError(Exception):
     def __init__(self, columns, message="Error in number of columns in the input file."):
         self.cols = columns
